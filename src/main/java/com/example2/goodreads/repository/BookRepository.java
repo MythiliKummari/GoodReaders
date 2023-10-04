8499953491a453201b4f5f4bcafd3e81a4f09b52
MythiliKummari <myartzthili@gmail.com>
2023-10-04 14:04:45 +0530

    firstcommit

--- src/main/java/com/example2/goodreads/repository/BookRepository.java
+++ src/main/java/com/example2/goodreads/repository/BookRepository.java
@@ -8,5 +8,7 @@ public interface BookRepository {
 	ArrayList<Book> getBooks();
 	Book getBookById(int bookId);
 	Book addBook(Book book);
+	   Book updateBook(int bookId, Book book);
+		void deleteBook(int bookId);
 
 }
